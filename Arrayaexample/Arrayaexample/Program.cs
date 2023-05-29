6a9d569726a204d2801224c519771cdda37307ab
pbhongade <134709097+pbhongade@users.noreply.github.com>
2023-05-29 12:53:02 +0530

    Add files via upload

---
+++ Arrayaexample/Arrayaexample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrayaexample
+{
+     class Program
+    {
+
+        
+        public static void countFreq(int[] arr, int n)
+        {
+            bool[] a = new bool[n];
+
+        
+            for (int i = 0; i < n; i++)
+            {
+
+                
+                if (a[i] == true)
+                    continue;
+
+                
+                int count = 1;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        a[j] = true;
+                        count++;
+                    }
+                }
+                Console.WriteLine(arr[i] + " " + count);
+            }
+        }
+
+        
+        public static void Main(String[] args)
+        {
+            int[] arr = new int[] { 10, 20, 20, 10, 10, 20, 5, 20 };
+            int n = arr.Length;
+            countFreq(arr, n);
+        }
+    }
+
+    
+
+ 
+    }
+
